17001639b3e1d3e79db74dc0fd6e0165c8ba871f
epatters <ejpatters@gmail.com>
2019-01-12 21:59:49 -0800

    Proof of concept of box with ports.
    No interactivity or labels yet.

--- examples/simple-flow/index.tsx
+++ examples/simple-flow/index.tsx
@@ -1,35 +1,47 @@
+import * as _ from 'lodash';
 import * as React from 'react';
 import { render } from 'react-dom';
 import * as Konva from 'konva';
-import { Stage, Layer, Rect, Text } from 'react-konva';
+import { Stage, Layer, Group, Rect, Circle } from 'react-konva';
 
-class ColoredRect extends React.Component {
-  state = {
-    color: 'green'
-  };
-  handleClick = () => {
-    this.setState({
-      color: Konva.Util.getRandomColor()
-    });
-  };
-  render() {
-    return (
-      <Rect x={20} y={20} width={50} height={50}
-        fill={this.state.color}
-        shadowBlur={5}
-        onClick={this.handleClick}
-      />
-    );
-  }
+interface BoxProps extends Konva.NodeConfig {
+  nin: number;
+  nout: number;
 }
+
+const Box = (props: BoxProps) => {
+  const strokeWidth = 2;
+  const radius = 5;
+  const xOffset = radius + strokeWidth / 2;
+  const yOffset = strokeWidth / 2;
+  const inOffset = props.height / (props.nin + 1);
+  const outOffset = props.height / (props.nout + 1);
+  return (
+    <Group {...props}>
+      <Rect
+        x={xOffset} y={yOffset}
+        width={props.width-2*xOffset} height={props.height-2*yOffset}
+        cornerRadius={radius}
+        stroke="black" strokeWidth={strokeWidth} fill="lightgray"
+      />
+      {_.range(props.nin).map((_, i) =>
+        <Circle x={xOffset} y={(i+1) * inOffset}
+          radius={radius} stroke="black" fill="white" />
+      )}
+      {_.range(props.nout).map((_, i) =>
+        <Circle x={props.width-xOffset} y={(i+1) * outOffset}
+          radius={radius} stroke="black" fill="white" />
+      )}
+    </Group>
+  );
+};
 
 class App extends React.Component {
   render() {
     return (
       <Stage width={window.innerWidth} height={window.innerHeight}>
         <Layer>
-          <Text text="Click on rect" />
-          <ColoredRect />
+          <Box width={100} height={100} nin={3} nout={2} draggable />
         </Layer>
       </Stage>
     );
